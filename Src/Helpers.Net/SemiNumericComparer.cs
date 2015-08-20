4752cdd2006f9fb427848aeca247d4352dc75e1d
lenya <lenya@msn.cn>
2015-08-20 09:40:26 +0800

    新增数字型字符串比较器

---
+++ Src/Helpers.Net/SemiNumericComparer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace System
+{
+    //http://stackoverflow.com/questions/6396378/c-sharp-linq-orderby-numbers-that-are-string-and-you-cannot-convert-them-to-int
+    public class SemiNumericComparer : IComparer<string>
+    {
+        public int Compare(string s1, string s2)
+        {
+            if (IsNumeric(s1) && IsNumeric(s2))
+            {
+                if (Convert.ToInt32(s1) > Convert.ToInt32(s2)) return 1;
+                if (Convert.ToInt32(s1) < Convert.ToInt32(s2)) return -1;
+                if (Convert.ToInt32(s1) == Convert.ToInt32(s2)) return 0;
+            }
+
+            if (IsNumeric(s1) && !IsNumeric(s2))
+                return -1;
+
+            if (!IsNumeric(s1) && IsNumeric(s2))
+                return 1;
+
+            return string.Compare(s1, s2, true);
+        }
+
+        public static bool IsNumeric(object value)
+        {
+            try
+            {
+                int i = Convert.ToInt32(value.ToString());
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+    }
+}
